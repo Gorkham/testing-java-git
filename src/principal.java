6c7591ed66fb808198d8c0919c4bafd3a5a66f3c
Gorkham <113120841+Gorkham@users.noreply.github.com>
2023-05-17 22:36:10 -0400

    Actualice requerimiento del profe ross

--- src/principal.java
+++ src/principal.java
@@ -2,10 +2,12 @@
 public class principal {
 
 	public static void main(String[] args) {
-		holaMundo("Hola Mundo XD");	
+		for(int i = 0; i < 10; i++){
+			printMessage("Hola Mundo " + i);	
+		}
 	}
 
-	public static void holaMundo(String value) {
+	public static void printMessage(String value) {
 		System.out.println(value);
 	}
 }
