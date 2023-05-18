1cea36f79c9c2f99c0a617d31e925bf17b05e58b
Gorkham <cristianurqueta23@gmail.com>
2023-05-17 22:37:55 -0400

    Modifique el main

--- src/principal.java
+++ src/principal.java
@@ -3,9 +3,12 @@ public class principal {
 
 	public static void main(String[] args) {
 		holaMundo("Hola Mundo XD");	
+		holaMundo("Empanada");
+		holaMundo("Completo");
 	}
 
 	public static void holaMundo(String value) {
-		System.out.println(value);
+		System.out.println(value + 2);
 	}
+	
 }
