f35fea7481f3f7665661b414422a142616ae6772
Gorkham <113120841+Gorkham@users.noreply.github.com>
2023-05-17 22:47:49 -0400

    Update principal.java

--- src/principal.java
+++ src/principal.java
@@ -3,20 +3,10 @@ public class principal {
 
 	public static void main(String[] args) {
 		holaMundo("Hola Mundo XD");	
-		holaMundo("Empanada");
-		holaMundo("Completo");
-		
-		for(int i = 0; i < 10; i++){
-			printMessage("Hola Mundo " + i);	
-		}
 	}
 
 	public static void holaMundo(String value) {
 		System.out.println(value + 2);
-	}
-
-	public static void printMessage(String value) {
-		System.out.println(value);
 	}
 	
 }
